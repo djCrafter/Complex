de5ce4aec939aceafc3aab1b4eaa68fb4613a56a
Oleksii Matsapura <godhedin2000@gmail.com>
2019-02-07 17:07:58 +0200

    C1

---
+++ Complex/Complex/Complex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Complex
+{
+    class Complex
+    {
+        double real, i;
+
+        public Complex()
+        {
+            this.real = 0.0;
+            this.i = 0.0;
+        }
+
+        public Complex(double real, double i)
+        {
+            this.real = real;
+            this.i = i;
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            Complex res = new Complex();
+            res.real = a.real + b.real;
+            res.i = a.i + b.i;
+            return res;
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            Complex res = new Complex();
+            res.real = a.real - b.real;
+            res.i = a.i - b.i;
+            return res;
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            Complex res = new Complex();
+            res.real = a.real * b.real - a.i * b.i;
+            res.i = a.i * b.real + a.real * b.i;
+            return res;
+        }
+
+        public static Complex operator /(Complex a, Complex b)
+        {
+            double Denominator = b.real * b.real + b.i * b.i;
+            return new Complex((a.real * b.real + a.i * b.i) / Denominator,
+                (b.real * a.i - b.i * a.i) / Denominator);
+        }
+
+        public static bool operator ==(Complex a, Complex b)
+        {
+            return a.real == b.real && a.i == b.i;
+        }
+
+        public static bool operator !=(Complex c1, Complex c2)
+        {
+            return c1.real != c2.real || c1.i != c2.i;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} + i{1}", this.real, this.i);
+        }
+    }
+
+}
---
+++ Complex/Complex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(5, 4);
+            Complex b = new Complex(6, 8);
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a == b);
+
+            Console.ReadLine();
+        }
+    }
+}
